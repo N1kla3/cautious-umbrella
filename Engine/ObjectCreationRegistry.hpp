d94a6f4ec5bd70cfd7ece55d95efaf9e2b455f62
Nicola <kolya.vladimirsky@gmail.com>
2020-12-29 21:04:31 +0300

    Creation registry

---
+++ Engine/ObjectCreationRegistry.hpp
@@ -0,0 +1,45 @@
+//
+// Created by nicola on 29/12/2020.
+//
+#pragma once
+
+#include "GameObject.h"
+
+typedef GameObject* (*GameObjectCreationFunc)();
+
+class ObjectCreationRegistry {
+public:
+    static ObjectCreationRegistry& Get()
+    {
+        static ObjectCreationRegistry sInstance;
+        return sInstance;
+    }
+
+    template<class T>
+    void RegisterCreationFunction()
+    {
+        assert(m_NameToGameObjectCreationFunctionMap.find(T::ClassId)
+               == m_NameToGameObjectCreationFunctionMap.end());
+        m_NameToGameObjectCreationFunctionMap[T::ClassId] = T::CreateInstance;
+    }
+
+    GameObject* CreateGameObject(uint32_t ClassId)
+    {
+        GameObjectCreationFunc creationFunc = m_NameToGameObjectCreationFunctionMap[ClassId];
+        if (creationFunc) {
+            GameObject *gameObject = creationFunc();
+            return gameObject;
+        }
+        return nullptr;
+    }
+
+private:
+    ObjectCreationRegistry(){}
+    std::unordered_map<uint32_t, GameObjectCreationFunc> m_NameToGameObjectCreationFunctionMap;
+};
+
+void RegisterObjectCreation()
+{
+    ObjectCreationRegistry::Get().RegisterCreationFunction<GameObject>();
+}
+
---
+++ Tests/testCreationRegistry.cpp
@@ -0,0 +1,15 @@
+//
+// Created by nicola on 29/12/2020.
+//
+#include "gtest/gtest.h"
+#include "../Engine/ObjectCreationRegistry.hpp"
+#include "../Engine/GameObject.h"
+
+TEST(CreationRegistry, initilization){
+    ObjectCreationRegistry::Get();
+    RegisterObjectCreation();
+    auto obj = ObjectCreationRegistry::Get().CreateGameObject('GOBJ');
+    auto objnul = ObjectCreationRegistry::Get().CreateGameObject('asdg');
+    EXPECT_TRUE(obj);
+    EXPECT_FALSE(objnul);
+}
