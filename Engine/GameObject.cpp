60f0309d23390e4152791e596ed61829e9eebe6b
Nicola <kolya.vladimirsky@gmail.com>
2021-02-10 00:35:27 +0300

    game object props

--- Engine/GameObject.cpp
+++ Engine/GameObject.cpp
@@ -4,10 +4,121 @@
 
 #include "GameObject.h"
 
-void GameObject::Write(OutputMemoryBitStream &inStream) {
+void GameObject::Write(OutputMemoryBitStream& inStream)
+{
 //TODO: write later
 }
 
-void GameObject::Read(InputMemoryBitStream &outStream) {
+void GameObject::Read(InputMemoryBitStream& outStream)
+{
 //TODO: read later
+}
+
+GameObject::GameObject()
+        : m_CollisionRadius(0.5f),
+          m_DoesWantToDie(false),
+          m_IndexInWorld(-1),
+          m_NetworkId(0),
+          m_Rotation(0.f),
+          m_Scale(1.f),
+          m_Color(Colors::White)
+{
+
+}
+
+uint32_t GameObject::GetAllStateMask() const
+{
+    return 0;
+}
+
+void GameObject::Update()
+{
+
+}
+
+void GameObject::HandleDying()
+{
+
+}
+
+GameObject::~GameObject() = default;
+
+const Vector3& GameObject::GetLocation() const
+{
+    return m_Location;
+}
+
+void GameObject::SetLocation(const Vector3& vector) noexcept
+{
+    m_Location = vector;
+}
+
+const Vector3& GameObject::GetColor() const
+{
+    return m_Color;
+}
+
+void GameObject::SetColor(const Vector3& color) noexcept
+{
+    m_Color = color;
+}
+
+float GameObject::GetCollisionRadius() const
+{
+    return m_CollisionRadius;
+}
+
+void GameObject::SetCollisionRadius(float radius) noexcept
+{
+    m_CollisionRadius = radius;
+}
+
+float GameObject::GetRotation() const
+{
+    return m_Rotation;
+}
+
+void GameObject::SetRotation(float rotation) noexcept
+{
+    m_Rotation = rotation;
+}
+
+float GameObject::GetScale() const
+{
+    return m_Scale;
+}
+
+void GameObject::SetScale(float scale) noexcept
+{
+    m_Scale = scale;
+}
+
+int GameObject::GetWorldIndex() const
+{
+    return m_IndexInWorld;
+}
+
+void GameObject::SetWorldIndex(int worldIndex) noexcept
+{
+    m_IndexInWorld = worldIndex;
+}
+
+int GameObject::GetNetworkId() const
+{
+    return m_NetworkId;
+}
+
+void GameObject::SetNetworkId(int networkId) noexcept
+{
+    m_NetworkId = networkId;
+}
+
+bool GameObject::DoesWantToDie() const
+{
+    return m_DoesWantToDie;
+}
+
+void GameObject::SetDoesWantToDie(bool value) noexcept
+{
+    m_DoesWantToDie = value;
 }
--- Engine/GameObject.h
+++ Engine/GameObject.h
@@ -4,7 +4,9 @@
 #pragma once
 
 #include <cstdint>
+#include <memory>
 #include "MemoryStream.h"
+#include "RoboMath.h"
 
 #define CLASS_IDENTIFICATION(Code, Class) \
 enum{ClassId = Code};                     \
@@ -15,8 +17,55 @@ class GameObject {
 public:
     CLASS_IDENTIFICATION('GOBJ', GameObject)
 
+    GameObject();
+    virtual ~GameObject();
+
+    virtual uint32_t GetAllStateMask() const;
+    virtual void Update();
+    virtual void HandleDying();
+
+    //Getters and setters
+    [[nodiscard]] const Vector3& GetLocation() const;
+    void SetLocation(const Vector3& vector) noexcept;
+
+    [[nodiscard]] const Vector3& GetColor() const;
+    void SetColor(const Vector3& color) noexcept;
+
+    [[nodiscard]] float GetCollisionRadius() const;
+    void SetCollisionRadius(float radius) noexcept;
+
+    [[nodiscard]] float GetRotation() const;
+    void SetRotation(float rotation) noexcept;
+
+    [[nodiscard]] float GetScale() const;
+    void SetScale(float scale) noexcept;
+
+    [[nodiscard]] int GetWorldIndex() const;
+    void SetWorldIndex(int worldIndex) noexcept;
+
+    [[nodiscard]] int GetNetworkId() const;
+    void SetNetworkId(int networkId) noexcept;
+
+    [[nodiscard]] bool DoesWantToDie() const;
+    void SetDoesWantToDie(bool value) noexcept;
+
     virtual void Write(OutputMemoryBitStream& inStream);
 
     virtual void Read(InputMemoryBitStream& outStream);
+
+private:
+    Vector3 m_Location{};
+    Vector3 m_Color;
+
+    float m_CollisionRadius;
+
+    float m_Rotation;
+    float m_Scale;
+
+    int m_IndexInWorld;
+    int m_NetworkId;
+
+    bool m_DoesWantToDie;
 };
 
+typedef std::shared_ptr<GameObject> SharedGameObject;
